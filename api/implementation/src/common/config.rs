36c6279d8dd5e20d74e4e121b8fa9502da31390e
Ebuka Umeokonkwo <ebukaume@gmail.com>
2023-11-01 19:28:19 +0000

    improve ci time

--- api/implementation/src/common/config.rs
+++ api/implementation/src/common/config.rs
@@ -32,8 +32,9 @@ impl Config {
     pub fn new() -> Self {
         dotenv::dotenv().ok();
         let app = Application {
-            name: env::var("APP_NAME").unwrap_or_else(|_| { panic!("{}", error_message("APP_NAME")) }),
-            version: env::var("APP_VERSION").unwrap_or_else(|_| { panic!("{}", error_message("APP_VERSION")) }),
+            name: env::var("APP_NAME").unwrap_or_else(|_| panic!("{}", error_message("APP_NAME"))),
+            version: env::var("APP_VERSION")
+                .unwrap_or_else(|_| panic!("{}", error_message("APP_VERSION"))),
         };
         Self {
             port: env::var("PORT")
@@ -41,18 +42,20 @@ impl Config {
                 .parse()
                 .expect("PORT must be a number"),
             env: env::var("ENV")
-                .unwrap_or_else(|_| { panic!("{}", error_message("ENV")) })
+                .unwrap_or_else(|_| panic!("{}", error_message("ENV")))
                 .to_lowercase()
                 .as_str()
                 .into(),
-            db_url: env::var("SURREALDB_URL").unwrap_or_else(|_| { panic!("{}", error_message("SURREALDB_URL")) }),
+            db_url: env::var("SURREALDB_URL")
+                .unwrap_or_else(|_| panic!("{}", error_message("SURREALDB_URL"))),
             db_namespace: env::var("SURREALDB_NAMESPACE")
-                .unwrap_or_else(|_| { panic!("{}", error_message("SURREALDB_NAMESPACE")) }),
-            db_name: env::var("SURREALDB_DATABASE").unwrap_or_else(|_| { panic!("{}", error_message("SURREALDB_DATABASE")) }),
+                .unwrap_or_else(|_| panic!("{}", error_message("SURREALDB_NAMESPACE"))),
+            db_name: env::var("SURREALDB_DATABASE")
+                .unwrap_or_else(|_| panic!("{}", error_message("SURREALDB_DATABASE"))),
             db_username: env::var("SURREALDB_USERNAME")
-                .unwrap_or_else(|_| { panic!("{}", error_message("SURREALDB_USERNAME")) }),
+                .unwrap_or_else(|_| panic!("{}", error_message("SURREALDB_USERNAME"))),
             db_password: env::var("SURREALDB_PASSWORD")
-                .unwrap_or_else(|_| { panic!("{}", error_message("SURREALDB_PASSWORD")) }),
+                .unwrap_or_else(|_| panic!("{}", error_message("SURREALDB_PASSWORD"))),
             app,
         }
     }
